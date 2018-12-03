273e9b9658480bfb2a593e94ccb7e2d547a565a5
Constantinos Karaolis <kostaskaraolis@gmail.com>
2018-12-03 21:16:18 +0200

    fixed port forwarding

--- setup.rb
+++ setup.rb
@@ -22,9 +22,10 @@ class Build
     end
 
     # Configure Port Forwarding To The Box
-    config.vm.network "forwarded_port", guest: 80, host: 8000
-    config.vm.network "forwarded_port", guest: 443, host: 44300
-    config.vm.network "forwarded_port", guest: 3306, host: 33060
+    config.vm.network "forwarded_port", guest: 4520, host: 60500
+    # config.vm.network "forwarded_port", guest: 80, host: 8000
+    # config.vm.network "forwarded_port", guest: 443, host: 44300
+    # config.vm.network "forwarded_port", guest: 3306, host: 33060
 
     # Add Custom Ports From Configuration
     if settings.has_key?("ports")
